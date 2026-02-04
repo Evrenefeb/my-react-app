b83ff2b1012ce1909a046119e7de36eee1318d49
fatihb4rat-jpg <fatihb4rat@gmail.com>
2026-02-04 16:52:26 +0300

    forms bracnh

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,17 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import UserTable from './UserTable';
+import Forms from './assets/components/forms';
 
 function App() {
   
   return(
     <>
       <UserTable />
+      <Forms />
     </>
     
   );
   
 }
-
+// deneme
 export default App
